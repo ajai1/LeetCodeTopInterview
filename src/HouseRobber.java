36a49364b3a5b91825242256275d10a0afc444c4
ajai1 <emailtoajai@gmail.com>
2020-05-29 17:29:39 +0530

    House Robber DP solution

--- src/HouseRobber.java
+++ src/HouseRobber.java
@@ -4,10 +4,18 @@ public class HouseRobber {
 	
     public static int rob(int[] nums) {
         
+    	int[] dp = new int[nums.length+1];
+    	dp[0] = 0;
+    	dp[1] = nums[0];
+    	for(int i = 1; i<nums.length; i++) {
+    		dp[i+1] = Math.max(dp[i], (dp[i-1] + nums[i]));
+    	}
+    	return dp[dp.length-1];
     }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] nums = {2,7,9,3,1};
+		System.out.println(rob(nums));
 
 	}
 
